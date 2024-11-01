22928b969c0ecafff4b582cd6c09b0fe15ac9861
Aritra Chakraborty <aritrac695@gmail.com>
2024-11-01 17:15:30 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is java program ");
+        System.out.println("Output from java programs and testing jenkins");
+        System.out.println(new Date());
+    }
+}
